1c2645ff930d7454fdee385af07d4cb8a6c898aa
HoaPQ <hoapq@HoaPQs-iMac.local>
2020-07-11 09:55:34 +0700

    Version 0.0.1

---
+++ MBSegmentControl/MBSegmentControl.h
@@ -0,0 +1,19 @@
+//
+//  MBSegmentControl.h
+//  MBSegmentControl
+//
+//  Created by HoaPQ on 7/11/20.
+//  Copyright © 2020 HoaPQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBSegmentControl.
+FOUNDATION_EXPORT double MBSegmentControlVersionNumber;
+
+//! Project version string for MBSegmentControl.
+FOUNDATION_EXPORT const unsigned char MBSegmentControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBSegmentControl/PublicHeader.h>
+
+
